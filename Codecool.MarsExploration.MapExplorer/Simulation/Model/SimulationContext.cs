41f48379c201cc1aa5929e62bbed4f039b8fa360
norbert <kulcsar.norbert.86@gmail.com>
2023-08-08 16:56:16 +0200

    [Feature] SimulationContext.cs -> Done

---
+++ Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
@@ -0,0 +1,6 @@
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Simulation.Model;
+
+public record SimulationContext(int TimeOutSteps, MarsRover.Model.MarsRover Rover, Coordinate LandingSpot, Map Map, IEnumerable<string> Resources, bool OutCome, int Steps = 0);
